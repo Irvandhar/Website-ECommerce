e50b65c7a0f49e77396874c2854adad23a39e7a6
Irvandharsyah Madiyatama <91834108+Irvandhar@users.noreply.github.com>
2023-11-26 15:30:32 +0700

    Delete WarungLaptop/WarungLaptop directory

--- WarungLaptop/WarungLaptop/db_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "logreg";
-
-    $conn = new mysqli($servername, $username, $password, $database);
-
-?>
--- WarungLaptop/WarungLaptop/db_logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-    session_start();// session dimulai
-    session_destroy();// hapus session
-    header("location: login.php");// redirect ke halaman index.php
-?>
